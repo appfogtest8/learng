0009ed8038ed2b493e659805da04d1f1d2571c06
app fogtest <appfogtest8@mm.st>
2020-07-24 10:46:20 -0700

    Ch 7: Filling in the blanks

--- app/src/main/java/com/exampler/listmaker/ListSelectionRecyclerViewAdapter.kt
+++ app/src/main/java/com/exampler/listmaker/ListSelectionRecyclerViewAdapter.kt
@@ -4,12 +4,15 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class ListSelectionRecyclerViewAdapter : RecyclerView.Adapter<ListSelectionViewHolder>() {
+
+   val listTitles = arrayOf("Shopping List", "Chores", "Android Tutorials")
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListSelectionViewHolder {
         TODO("Not yet implemented")
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return listTitles.size
     }
 
     override fun onBindViewHolder(holder: ListSelectionViewHolder, position: Int) {

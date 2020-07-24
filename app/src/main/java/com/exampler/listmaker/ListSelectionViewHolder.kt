bca05408bc097b4ccbf74424e43dc5e3b082a005
app fogtest <appfogtest8@mm.st>
2020-07-24 10:43:39 -0700

    Ch 7: Setting up a RecyclerView Adapter

---
+++ app/src/main/java/com/exampler/listmaker/ListSelectionViewHolder.kt
@@ -0,0 +1,7 @@
+package com.exampler.listmaker
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class ListSelectionViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+}

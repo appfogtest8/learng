f5faa4f57ab54c3e6be0555d4f398e33748d8d8e
app fogtest <appfogtest8@mm.st>
2020-07-29 22:40:37 -0700

    Ch 9: Creating a new Activity

---
+++ app/src/main/java/com/exampler/listmaker/ListDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.exampler.listmaker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_detail)
+    }
+}

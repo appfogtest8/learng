e8a8ef0f6dcf6f22733f6bcacf1dbfb871a888fb
app fogtest <appfogtest8@mm.st>
2020-07-30 18:35:53 -0700

    Ch 9: Intents

--- app/src/main/java/com/exampler/listmaker/MainActivity.kt
+++ app/src/main/java/com/exampler/listmaker/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.exampler.listmaker
 
+import android.content.Intent
 import android.os.Bundle
 import android.text.InputType
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -74,5 +75,14 @@ class MainActivity : AppCompatActivity() {
         }
 
         builder.create().show()
+    }
+
+    private fun  showListDetail(list: TaskList) {
+
+        val listDetailIntent = Intent(this, ListDetailActivity::class.java)
+
+        listDetailIntent.putExtra(INTENT_LIST_KEY, list)
+
+        startActivity(listDetailIntent)
     }
 }
